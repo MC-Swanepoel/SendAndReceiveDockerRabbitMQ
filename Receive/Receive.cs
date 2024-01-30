12979de8817daca1813133e9e2768f45010222c2
MartinSwanepoel <MartinSwanepoel@DESKTOP-6O5RA4G>
2024-01-30 13:28:55 +0200

    Add project files.

---
+++ Receive/Receive.cs
@@ -0,0 +1,53 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory { HostName = "rabbitmq", UserName = "guest", Password = "guest" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+//tutorial 1
+//channel.QueueDeclare(queue: "hello",
+//                     durable: false,
+//                     exclusive: false,
+//                     autoDelete: false,
+//                     arguments: null);
+
+//Console.WriteLine(" [*] Waiting for messages.");
+
+//var consumer = new EventingBasicConsumer(channel);
+//consumer.Received += (model, ea) =>
+//{
+//    var body = ea.Body.ToArray();
+//    var message = Encoding.UTF8.GetString(body);
+//    Console.WriteLine($" [x] Received {message}");
+//};
+//channel.BasicConsume(queue: "hello",
+//                     autoAck: true,
+//                     consumer: consumer);
+
+//Console.WriteLine(" Press [enter] to exit.");
+//Console.ReadLine();
+
+channel.ExchangeDeclare("logs", ExchangeType.Fanout);
+
+// declare a server-named queue
+var queueName = channel.QueueDeclare().QueueName;
+channel.QueueBind(queue: queueName,
+                  exchange: "logs",
+                  routingKey: string.Empty);
+Console.WriteLine(" [*] Waiting for logs.");
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, ea) =>
+{
+    byte[] body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($" [x] {message}");
+};
+channel.BasicConsume(queue: queueName,
+                     autoAck: true,
+                     consumer: consumer);
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
---
+++ Send/Send.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory { HostName = "rabbitmq", UserName = "guest", Password = "guest" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+//tutorial 1
+//channel.QueueDeclare(queue: "hello",
+//                     durable: false,
+//                     exclusive: false,
+//                     autoDelete: false,
+//                     arguments: null);
+
+//const string message = "Hello World!";
+//var body = Encoding.UTF8.GetBytes(message);
+//channel.BasicPublish(exchange: string.Empty,
+//                     routingKey: "hello",
+//                     basicProperties: null,
+//                     body: body);
+
+channel.ExchangeDeclare("logs", ExchangeType.Fanout);
+
+var message = GetMessage(args);
+var body = Encoding.UTF8.GetBytes(message);
+channel.BasicPublish(exchange: "logs",
+                     routingKey: string.Empty,
+                     basicProperties: null,
+                     body: body);
+
+Console.WriteLine($" [x] Sent {message}");
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
+
+static string GetMessage(string[] args)
+{
+    return ((args.Length > 0) ? string.Join(" ", args) : "info: Hello World!");
+}
